608f905641ee1573faa12758b77002b2a30236d3
Shobhit Pundir <er.shobhitpundir@gmail.com>
2023-04-16 07:20:14 +0530

    Chapter 3

--- App.js
+++ App.js
@@ -1,42 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    {id:'parent'},
-    [React.createElement(
-        "div",
-        {id:'child1'},
-         [
-            React.createElement(
-            "h1",
-            {},
-            "Hello World from h1"
-            ),
-            React.createElement(
-                "h2",
-                {},
-                "Hello World from h2"
-                )
-         ]
-        ),
-        React.createElement(
-            "div",
-            {id:'child2'},
-             [
-                React.createElement(
-                "h1",
-                {},
-                "Hello World from h1"
-                ),
-                React.createElement(
-                    "h2",
-                    {},
-                    "Hello World from h2"
-                    )
-             ]
-            )]
-    );
+// Reach Element
+const heading = <h1> This is React Element</h1>;
+
+// React Component
+const Heading = () => {
+  return <h1> This is main heading.</h1>;
+};
+
+/*
+const HeadingComponent = () => {
+    return <h1> This is my Component </h1>
+}
+*/
+// const HeadingComponent = () => <h1> This is my Component </h1>
+
+const HeadingComponent = () => (
+  // <React.Fragment>
+  <>
+    <div className="head">
+      <Heading />
+      <Heading> </Heading>
+      {Heading()}
+      <h1> This is my functional Component </h1>
+    </div>
+    <div className="head">
+        {heading}
+      <h1> This is Second Div</h1>
+    </div>
+  </>
+);
+
 // console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
